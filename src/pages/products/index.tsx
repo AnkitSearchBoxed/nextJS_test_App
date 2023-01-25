a3204b64ebd9739d8ae51656ee0389eeff5d62eb
ankitsinghsb <ankit@searchboxed.com>
2023-01-25 15:27:25 +0530

    used grid

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -8,16 +8,21 @@ const productListPage = (props: SSRProps) => {
   return (
     <>
       <h1 className="text-red-400 text-center text-2xl">Product list</h1>
-      {props.product.map((item: any) => (
-        <>
-          <div className="text-center cursor-pointer">
-            <img className="w-52 m-auto" src={item.image} alt={item.title} />
-            <p>Title: {item.title.slice(1, 17).toUpperCase()}</p>
-            <p>Price: {item.price} /-</p>
-          </div>
-        </>
-      ))}
-      <div className="border border-grey-800 max-w-xl  mt-20 flex "></div>
+      <div className="border border-grey-800 p-5 m-5 m-auto  mt-20 grid grid-cols-3 gap-1">
+        {props.product.map((item: any) => (
+          <>
+            <div className=" border border-grey-600 text-center cursor-pointer ">
+              <img
+                className="w-52 h-52 m-5 p-5 m-auto "
+                src={item.image}
+                alt={item.title}
+              />
+              <p>Title: {item.title.slice(1, 17).toUpperCase()}</p>
+              <p>Price: {item.price} /-</p>
+            </div>
+          </>
+        ))}
+      </div>
     </>
   );
 };
