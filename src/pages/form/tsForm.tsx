63a25e4994dab493e5dd1f69e37b85b69ef7149a
ankitsinghsb <ankit@searchboxed.com>
2023-01-26 16:27:50 +0530

    tried components like header and features

--- src/pages/form/tsForm.tsx
+++ src/pages/form/tsForm.tsx
@@ -1,15 +1,19 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { date } from "yup";
 interface IFormInputs {
-  firstName: string;
+  name: string;
   age: number;
+  url: string;
 }
 
 const schema = yup
   .object({
-    firstName: yup.string().required(),
+    name: yup.string().required(),
     age: yup.number().positive().integer().required(),
+    url: yup.string().url().nullable(),
+    createdOn: date().default(() => new Date()),
   })
   .required();
 
@@ -29,23 +33,39 @@ export default function TsForm() {
     <>
       <div className="m-auto mt-20 border  text-center ">
         <form onSubmit={handleSubmit(onSubmit)} className=" mt-5">
-          <label>Name: </label>
-          <input
-            className="border border-black rounded mb-2 placeholder-black placeholder-opacity-60"
-            {...register("firstName")}
-            placeholder="enter your name"
-          />
-          <p>{errors.firstName?.message}</p>
+          <div className="flex justify-center">
+            <label>Name: </label>
+            <input
+              className="ml-5 border border-black rounded mb-2 placeholder-black placeholder-opacity-60 text-center"
+              {...register("name")}
+              placeholder="enter name..."
+            />
+            <p>{errors.name?.message}</p>
+          </div>
           <br />
-          <label>Age: </label>
+          <div className="flex justify-center">
+            <label>Age: </label>
+            <input
+              className="ml-5 border border-black rounded mb-2 placeholder-black placeholder-opacity-60 text-center"
+              {...register("age")}
+              placeholder="enter age..."
+            />
+            <p>{errors.age?.message}</p>
+          </div>
+          <br />
+          <div className="flex justify-center">
+            <label>Url: </label>
+            <input
+              className="ml-5 border border-black rounded mb-2 placeholder-black placeholder-opacity-60 text-center"
+              {...register("url")}
+              placeholder="enter url..."
+            />
+            <p>{errors.url?.message}</p>
+          </div>
+          <br />
+
           <input
-            className="border border-black rounded mb-2 placeholder-black placeholder-opacity-60"
-            {...register("age")}
-            placeholder="enter your age"
-          />
-          <p>{errors.age?.message}</p>
-          <input
-            className="border border-black rounded p-1 m-1 bg-blue-400 mb-2 cursor-pointer"
+            className="border border-black rounded p-1 m-1 bg-blue-400 mb-2 cursor-pointer "
             type="submit"
           />
         </form>
